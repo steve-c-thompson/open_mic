a70ea0c03ba3dacd99ba5e46bdf525ef2ebe082b
Steve Thompson <scthomps312@gmail.com>
2018-06-24 19:59:37 -0600

    Joke now has initialize with hash; tests for creation and init

--- lib/joke.rb
+++ lib/joke.rb
@@ -1,3 +1,11 @@
 class Joke
-
+  attr_accessor :id
+  attr_accessor :question
+  attr_accessor :answer
+  
+  def initialize(joke_data)
+    @id = joke_data[:id]
+    @question = joke_data[:question]
+    @answer = joke_data[:answer]
+  end
 end
--- test/joke_test.rb
+++ test/joke_test.rb
@@ -4,5 +4,14 @@ require 'minitest/pride'
 require './lib/joke'
 
 class JokeTest < Minitest::Test
-
+  def test_joke_exists
+    joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
+    assert_instance_of(Joke, joke)
+  end
+  def test_joke_initialized_with_hash_params
+    joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
+    assert_equal(joke.id, 1)
+    assert_equal(joke.question, "Why did the strawberry cross the road?")
+    assert_equal(joke.answer, "Because his mother was in a jam.")
+  end
 end
