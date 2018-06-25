1805c9f1c350957f10a335455775b1853eab94b2
Steve Thompson <scthomps312@gmail.com>
2018-06-24 20:10:28 -0600

    Users can now add jokes (with tests). Also, excluding adding nil jokes because that wouldn't be funny.

--- lib/user.rb
+++ lib/user.rb
@@ -1,7 +1,16 @@
 class User
   attr_reader :name
+  attr_reader :jokes
 
   def initialize(name)
     @name = name
+    @jokes = []
   end
+
+  def learn(joke)
+    # Take advantage of if's truthiness and exclude nil jokes
+    @jokes.push(joke) if joke
+  end
+
+
 end
--- test/user_test.rb
+++ test/user_test.rb
@@ -21,4 +21,26 @@ class UserTest < Minitest::Test
 
     assert_equal "Ali", ali.name
   end
+
+  def test_it_is_initialized_with_empty_array_of_jokes
+    sal = User.new("Sal")
+    assert_instance_of Array, sal.jokes
+    assert_equal 0, sal.jokes.length
+  end
+
+  def test_it_can_learn_jokes
+    sal = User.new("Sal")
+    joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
+    sal.learn(joke)
+    refute_nil sal.jokes
+    assert_equal 1, sal.jokes.length
+    refute_nil sal.jokes[0]
+  end
+
+  def test_it_can_learn_only_non_nil_jokes
+    sal = User.new("Sal")
+    sal.learn(nil)
+    refute_nil sal.jokes
+    assert_equal 0, sal.jokes.length
+  end
 end
